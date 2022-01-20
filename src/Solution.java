e1fb471537b69f791ec3cd6105483d8efc587ec9
Isaac Ibanez Marquez <isaacibanez00@gmail.com>
2022-01-20 23:09:14 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    // Write a Java method to find the smallest number among three numbers.
+    //
+    //Test Data:
+    //Input the first number: 25
+    //Input the Second number: 37
+    //Input the third number: 29
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        double x = in.nextDouble();
+        System.out.print("Input the Second number: ");
+        double y = in.nextDouble();
+        System.out.print("Input the third number: ");
+        double z = in.nextDouble();
+        System.out.print("The smallest value is " + smallest(x, y, z)+"\n" );
+    }
+
+    public static double smallest(double x, double y, double z)
+    {
+        return Math.min(Math.min(x, y), z);
+    }
+
+    }
+
